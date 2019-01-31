5c65770086f0629e5ecaa2633049cb076f35db18
EC2 Default User <ec2-user@ip-172-31-14-182.us-east-2.compute.internal>
2019-01-31 13:42:42 +0000

    hello

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,6 @@
 class HangpersonGame
 
+  attr_accessor :word, :guesses, :wrong_guesses
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -10,6 +11,8 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
